7a06da9018a9f4c58dbfd75b9a4b7a78afdb5a86
srikanthpragada <srikanthpragada@gmail.com>
2018-07-07 08:30:30 +0530

    07-JULY

--- demo/models.py
+++ demo/models.py
@@ -9,3 +9,24 @@ class Course:
         self.duration = duration
         self.fee = fee
         self.topics = topics
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=30)
+    location = models.CharField(max_length=30)
+
+    def __str__(self):
+        return "%s %s" % (self.name, self.location)
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=30)
+    job = models.CharField(max_length=50)
+    salary = models.IntegerField()
+    department = models.ForeignKey(Department, on_delete='cascade')
+
+    def __str__(self):
+        return "%s,%s,%d" % (self.name, self.job, self.salary)
+
+    class Meta:
+        db_table = 'Employees'   # Name to be used in database
